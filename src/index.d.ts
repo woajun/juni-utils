2370b0d9a4c0e3b4f0750b60ec9f4994174313d4
juni <woajun@naver.com>
2024-03-27 10:39:36 +0900

    feat: 타입정의 추가

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,21 @@
-// index.d.ts
-export type DateFormat =
-  | 'D'
-  | 'M월D일'
-  | 'MM월DD일'
-  | 'MM/DD'
-  | 'M. D (AAA)'
-  | 'M.D'
-  | 'MM.DD'
-  | 'HH:MM'
-  | 'YY.MM.DD'
-  | 'YYYY-M-D'
-  | 'YYYY-MM-DD'
-  | 'YYYY. M. D (AAA)'
-  | 'MM/DD HH:MM'
-  | 'MM월DD일 HH:MM'
-  | '오전오후 H:MM'
-  | '오전오후 H:MM:s초';
+// // index.d.ts
+// export type DateFormat =
+//   | 'D'
+//   | 'M월D일'
+//   | 'MM월DD일'
+//   | 'MM/DD'
+//   | 'M. D (AAA)'
+//   | 'M.D'
+//   | 'MM.DD'
+//   | 'HH:MM'
+//   | 'YY.MM.DD'
+//   | 'YYYY-M-D'
+//   | 'YYYY-MM-DD'
+//   | 'YYYY. M. D (AAA)'
+//   | 'MM/DD HH:MM'
+//   | 'MM월DD일 HH:MM'
+//   | '오전오후 H:MM'
+//   | '오전오후 H:MM:s초';
 
-export declare function formatDate(target: Date | string, format?: DateFormat): string;
-export declare function adjustDate(target: Date | string, amount: number): Date;
+// export declare function formatDate(target: Date | string, format?: DateFormat): string;
+// export declare function adjustDate(target: Date | string, amount: number): Date;
